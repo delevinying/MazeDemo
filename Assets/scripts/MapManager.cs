c73c72ccebea3d81f38fbade96c837356adbfa6b
delevinying <846353427@qq.com>
2017-09-12 20:07:44 +0800

    创建迷宫，数组prefab生成

---
+++ Assets/scripts/MapManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject black;
+	public GameObject yellow;
+
+	public  int startX;
+	public  int startY;
+
+	public int[,] mazeNum=new int[,]{
+		{1,0,1,0,1,0,1,0,1,0},
+		{0,1,0,1,0,1,0,1,0,1},
+		{1,0,1,0,1,0,1,0,1,0},
+		{0,1,0,1,0,1,0,1,0,1},
+		{1,0,1,0,1,0,1,0,1,0},
+		{0,1,0,1,0,1,0,1,0,1},
+		{1,0,1,0,1,0,1,0,1,0},
+		{0,1,0,1,0,1,0,1,0,1},
+		{1,0,1,0,1,0,1,0,1,0},
+		{0,1,0,1,0,1,0,1,0,1}
+	}; 
+
+	void Start () {
+		for (int y = 0; y < 10; y++)
+		{
+			for (int x = 0; x < 10; x++)
+			{
+				if (mazeNum [x, y] == 0) {
+					Instantiate (black, new Vector3 (startX + 2*(x - 1), startY + 2*(y - 1), 0), Quaternion.identity);
+				} else {
+					Instantiate (yellow, new Vector3 (startX + 2*(x - 1), startY + 2*(y - 1), 0), Quaternion.identity);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
